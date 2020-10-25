330aa15398fd3c8190205c6f82f033be17076e85
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-10-25 00:18:17 -0500

    seo edits

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || "",
+    // title: process.env.npm_package_name || "",
+    title: "Gil's Portfolio Website",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
