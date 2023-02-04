3fd22c7d10ee75810fb47c62c81ce6573c3d5926
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-02-04 16:54:04 -0500

    Attempting to Deploy SvelteKit App to Netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from "@sveltejs/adapter-auto"; NOTE: not using this for now.
+import adapter from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      edge: false,
+      split: false,
+    }),
+  },
 };
 
 export default config;
