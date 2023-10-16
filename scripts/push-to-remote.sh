b4c9f9187b2292eb7435a560dd9ec33e70f153e7
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-10-16 14:56:22 -0400

    Regular Auto-Commit at 14:56:22 on 10/16/2023

--- scripts/push-to-remote.sh
+++ scripts/push-to-remote.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd .
+cd /home/rabuttsurwa96/Web Development/Projects/Personal/My Sites --gsm/Submodule Dev Environments/website/
 git add .
 timestamp() {
     date +"at %H:%M:%S on %m/%d/%Y"
