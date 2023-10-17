0aeeb4b55f3c82a33aa876ff20e8291cdde3c73e
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-10-17 17:18:01 -0400

    Regular Auto-Commit at 17:18:01 on 10/17/2023

--- scripts/push-to-remote.sh
+++ scripts/push-to-remote.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-export GIT_SSH_COMMAND="ssh -i /home/rabuttsurwa96/.ssh/id_github"
-
 # Functions
 timestamp() {
     date +"at %H:%M:%S on %m/%d/%Y"
 }
 
+export GIT_SSH_COMMAND="ssh -i /home/rabuttsurwa96/.ssh/id_github"
 echo "Committing & Pushing to Remote"
 cd /home/rabuttsurwa96/Web\ Development/Projects/Personal/My\ Sites\ --gsm/Submodule\ Dev\ Environments/website/
 git add .
