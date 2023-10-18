7d556dc0aa7abb03124699388bcb44be266ed358
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-10-17 21:08:01 -0400

    Scheculed Auto-Commit at 21:08:01 on 17/10/2023

--- scripts/push-to-remote.sh
+++ scripts/push-to-remote.sh
@@ -10,4 +10,4 @@ echo "Committing & Pushing to Remote"
 cd /home/rabuttsurwa96/Web\ Development/Projects/Personal/My\ Sites\ --gsm/Submodule\ Dev\ Environments/website/
 git add .
 git commit --message "Scheculed Auto-Commit $(timestamp)"
-git push
+git push origin master
