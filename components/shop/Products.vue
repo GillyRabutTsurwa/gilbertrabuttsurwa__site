b5ad78d0df48ad669235e71cf88f9e726bb88bea
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-12-14 15:27:42 -0500

    fix: Duplicate Stripe Products on Load - i added logic that will not populate duplicate products - this works because i am using the Sanity ID as my Stripe ID - Stripe generates a new ID by default each time a product is created, and I kept on getting duplicates - so now with this, new products will only be created if they have a unique ID, coming from my Sanity data

--- components/shop/Products.vue
+++ components/shop/Products.vue
@@ -17,41 +17,37 @@ const stripe: Stripe = new Stripe(config?.stripe?.key, {
 const client = createClient({
   projectId: config.public.sanity.projectId,
   dataset: "production",
-  apiVersion: "2022-01-12", // use current date (YYYY-MM-DD) to target the latest API version
+  apiVersion: "2022-01-12",
   useCdn: true,
 });
 
-const builder = imageUrlBuilder(client);  //NOTE: ne marche pas pour le moment
+const builder = imageUrlBuilder(client);
 const urlFor = (source: any) => builder.image(source);
 
 const query: string = groq`*[_type == "product"]`;
 const { data: products } = await useSanityQuery(query);
 
-// NOTE: consult pages/blog/personal/posts/index.vue for a detailed explanation of this one line of code below
 const store: Store<"products", StateTree> = useProductsStore();
 store.products = products.value;
 
-const testProducts = store.products.map(async (currentProduct: Product) => {
-  /**
-   * NOTE: 
-   * this is the same logic as I used in the stripe checkout file to get the image url of a sanity document using @sanity/image-url
-   */
+store.products.forEach(async (currentProduct: Product) => {
+  const duplicateProduct = await stripe.products.retrieve(currentProduct._id);
+  if (duplicateProduct) return;
   const str = currentProduct.mainImage.asset._ref;
   const { baseUrl, projectId, dataset, source } = urlFor(str).options;
-  const products = await stripe.products.create({
+  await stripe.products.create({
+    id: currentProduct._id, //NOTE: using ID that comes from Sanity instead of Stripe's default product ID to keep things consistent
     name: currentProduct.name,
     description: currentProduct.description.toString(),
     default_price_data: {
       unit_amount: currentProduct.price,
       currency: currentProduct.currency
     },
-    // NOTE: this will get us the full url of our sanity document image. very useful
     images: [`${baseUrl}/images/${projectId}/${dataset}/${str.slice(6, str.length - 4)}.jpg`],
     url: `${config.public.client_url}/shop` //NOTE: ça changera
   });
   return products;
 });
-console.log(await testProducts);
 </script>
 
 <template>
