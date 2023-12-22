d40716dffc1f1056e28b0fa413ef6f713d5c1889
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-12-22 18:26:11 -0500

    Adding Logic To Automatically Push Commited Changes to Remote, If Any

--- scripts/push-to-remote.sh
+++ scripts/push-to-remote.sh
@@ -7,7 +7,7 @@ timestamp() {
 
 if [ "$(git diff --exit-code)" ]
 then
-    echo "You have some changes to add"
+    echo "You have some unstaged changes to add"
 elif [ "$(git diff --staged --exit-code)" ]
 then 
     echo "You have some staged changes to commit"
@@ -17,15 +17,9 @@ then
     cd /home/rabuttsurwa96/Web\ Development/Projects/Personal/My\ Sites\ --gsm/Submodule\ Dev\ Environments/gilbertrabuttsurwa.com/frontend
     git commit --message "Scheculed Auto-Commit $(timestamp)"
     git push origin master  
+elif [ "$(git rev-list --count --right-only @{u}...HEAD)" -gt 0 ]
+then
+    echo "Your local branch is ahead of the remote branch. Push needed."
 else
     echo "Your worktree is clean.... for now"
 fi
-
-
-
-
-# echo "Initiating Auto Staging & Committing"
-# cd /home/rabuttsurwa96/Web\ Development/Projects/Personal/My\ Sites\ --gsm/Submodule\ Dev\ Environments/website/
-# git add .
-# git commit --message "Scheculed Auto-Commit $(timestamp)"
-# git push origin master
