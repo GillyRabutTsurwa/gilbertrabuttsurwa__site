dc27b44d5245246c5e6afe83bada828fef61e4ba
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-12-22 17:57:32 -0500

    Scheculed Auto-Commit at 17:57:32 on 22/12/2023

--- scripts/push-to-remote.sh
+++ scripts/push-to-remote.sh
@@ -6,8 +6,26 @@ timestamp() {
 
 eval $(ssh-agent -s)
 ssh-add /home/rabuttsurwa96/.ssh/key_github
-echo "Initiating Auto Staging & Committing"
-cd /home/rabuttsurwa96/Web\ Development/Projects/Personal/My\ Sites\ --gsm/Submodule\ Dev\ Environments/website/
-git add .
-git commit --message "Scheculed Auto-Commit $(timestamp)"
-git push origin master
+
+if [ "$(git diff --exit-code)" ]
+then
+    echo "You have some changes to add"
+elif [ "$(git diff --staged --exit-code)" ]
+then 
+    echo "You have some staged changes to commit"
+    sleep 5s
+    cd /home/rabuttsurwa96/Web\ Development/Projects/Personal/My\ Sites\ --gsm/Submodule\ Dev\ Environments/gilbertrabuttsurwa.com/frontend
+    git commit --message "Scheculed Auto-Commit $(timestamp)"
+    git push origin master  
+else
+    echo "Your worktree is clean.... for now"
+fi
+
+
+
+
+# echo "Initiating Auto Staging & Committing"
+# cd /home/rabuttsurwa96/Web\ Development/Projects/Personal/My\ Sites\ --gsm/Submodule\ Dev\ Environments/website/
+# git add .
+# git commit --message "Scheculed Auto-Commit $(timestamp)"
+# git push origin master
