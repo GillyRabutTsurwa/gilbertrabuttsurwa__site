79c0e40e8c64e27a9d8d13caf22362e9b3cc30e8
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2024-03-27 23:12:23 -0400

    Added Marquee & Updated a Few Packages (For Testing)

---
+++ storyblok/Marquee.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h2>What I Use</h2>
+    <h3>Front-End</h3>
+    <Vue3Marquee>
+        <i class="devicon-html5-plain"></i>
+        <i class="devicon-css3-plain"></i>
+        <i class="devicon-javascript-plain"></i>
+        <i class="devicon-typescript-plain"></i>
+        <i class="devicon-vue-plain"></i>
+        <i class="devicon-nuxt-plain"></i>
+        <i class="devicon-svelte-plain"></i>
+        <i class="devicon-react-plain"></i>
+    </Vue3Marquee>
+    <h3>Back-End</h3>
+    <Vue3Marquee>
+        <i class="devicon-html5-plain"></i>
+        <i class="devicon-css3-plain"></i>
+        <i class="devicon-javascript-plain"></i>
+        <i class="devicon-typescript-plain"></i>
+        <i class="devicon-vue-plain"></i>
+        <i class="devicon-nuxt-plain"></i>
+        <i class="devicon-svelte-plain"></i>
+        <i class="devicon-react-plain"></i>
+    </Vue3Marquee>
+</template>
+
+
+<style scoped></style>
