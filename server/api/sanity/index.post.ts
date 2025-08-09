b0066afa2b60fee31971e1e97b9f7c236c277429
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-08-09 02:33:48 -0400

    Refined Sanity Webhook

--- server/api/sanity/index.post.ts
+++ server/api/sanity/index.post.ts
@@ -1,14 +1,25 @@
-import { createClient } from "@sanity/client";
 import type { H3Event } from "h3";
-
-const config = useRuntimeConfig();
-// const client = createClient({ projectId: config.public.sanity.projectId, dataset: "production", apiVersion: "2022-01-12", useCdn: false });
+import Post from "~/models/posts";
 
 export default defineEventHandler(async (event: H3Event) => {
-    const body = await readBody(event);
     const headers = await getRequestHeaders(event);
+    const post = await readBody(event);
     console.log("Sanity webhook reçu:");
     console.log(headers);
+    if (headers["sanity-operation"] === "create") {
+        console.log(`Adding new post with id: ${headers["sanity-document-id"]}`);
+        await Post.create(post.body);
+    } else if (headers["sanity-operation"] === "update") {
+        console.log(`Updating body of post with id: ${headers["sanity-document-id"]}`);
+        await Post.findByIdAndUpdate(headers["sanity-document-id"], { title: post.title, slug: post.slug.current, body: post.body });
+    } else if (headers["sanity-operation"] === "delete") {
+        console.log(`Deleting post with id: ${headers["sanity-document-id"]}`);
+        await Post.findByIdAndDelete(headers["sanity-document-id"]);
+    } else {
+        console.error("On peut pas le faire");
+    }
     console.log("======================================");
-    console.log(body);
+    console.log("======================================");
+    console.log("======================================");
+    console.log(post);
 });
